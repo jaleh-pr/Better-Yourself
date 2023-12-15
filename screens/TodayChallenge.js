0e739d85fe907bc0f0909aaa691358a471510a31
jaleh-pr <jaleh.p1984@gmail.com>
2023-12-15 17:59:25 +1100

    challenge-function

--- screens/TodayChallenge.js
+++ screens/TodayChallenge.js
@@ -1,14 +1,54 @@
 
-import React from 'react';
+import React ,{ useState } from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 
-
 const TodayChalleng = (props) => {
+
   const onLanNumProps = (MyScreenNum) => {
     props.onLanguageName("English");
     props.onScreenNum(MyScreenNum);
 }
+
+  const challengesData = require("../Data/challengEnglish.json");
+  const [myNumArray, setMyNumArray] = useState([]);
+  const [theMessage, setTheMessage ] = useState("");
+  const [myArrayLen, setMyArrayLen] = useState();
+
+  
+//let initialRndNum = Math.floor(0 + Math.random()* 4 );
+
+const generateRandomNum = () => {
+    let rndNum = Math.floor(0 + Math.random()* 7 );
+    setChallengeMessage(rndNum);
+    setMyNumArray(myNumArray => [...myNumArray, rndNum]);
+
+  if (myNumArray.indexOf(rndNum) < 0 && myArrayLen <8 ){
+      setChallengeMessage(rndNum);
+      setMyNumArray(myNumArray => [...myNumArray, rndNum]);
+      
+     // setMyArrayLen(myArrayLen+1);
+  } else if (myNumArray.indexOf(rndNum) > 0 && myArrayLen<8 ){
+     
+         setChallengeMessage(rndNum+1);
+        setMyNumArray(myNumArray => [...myNumArray, rndNum+1]);
+
+  }else if (myNumArray.indexOf(rndNum) > 0 && myArrayLen >= 8){
+
+      setMyNumArray ([]);
+      setMyNumArray(myNumArray => [...myNumArray, rndNum]);
+  }
+  setMyArrayLen(myNumArray.length);
+  console.log(myNumArray);
+  console.log(rndNum);
+};
+
+const setChallengeMessage = (selectedNumber) => {
+  const chaD = JSON.stringify(challengesData[selectedNumber].Decscription);
+  setTheMessage(chaD.replace(/['"]+/g, ""));
+  
+} ;
+
 
   return (
     <View style={styles.mainScreen}>
@@ -19,8 +59,22 @@ const TodayChalleng = (props) => {
                 </Text>
             </View>
             <TouchableOpacity
-                     onPress={() => onLanNumProps(0)}
+                     onPress={() => generateRandomNum()}
                      style={styles.button}
+                >
+                    <View>
+                        <Text>Start</Text> 
+                        
+                    </View>
+                </TouchableOpacity>
+
+                <View>
+                   <Text>{theMessage}</Text>
+                </View>
+           
+          
+            <TouchableOpacity
+                     onPress={() => onLanNumProps(0)}
                 >
                     <View>
                         <Text>Back</Text> 
@@ -37,6 +91,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         color: "black",
         paddingTop: 10,
-    }
+    },
+    button:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding:12,
+      marginTop:35,
+      borderColor:'#55c2da',
+      borderRadius:10,
+      borderWidth:1,
+      backgroundColor:'#55c2da'
+  }
 })
 export default TodayChalleng;
